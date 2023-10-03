e521f789167fbe278555f7227c4ccfffcfe37d2f
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2023-10-03 22:46:49 +0100

    :iphone: Made `Wallet` screen responsive

--- src/Pages/Wallet/index.tsx
+++ src/Pages/Wallet/index.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 import CopyToClipboard from "react-copy-to-clipboard";
 import {
   Container,
-  TextField,
+  Grid,
   TextFieldProps,
   Button,
   Alert,
@@ -16,24 +16,13 @@ import {
   MenuItem,
 } from "@mui/material";
 
-import DefaultUserImage from "../../Assets/IMG/DefaultUserImage.png";
-
-import { PerformRequest, UploadFile } from "../../Lib/PerformRequest";
+import { PerformRequest } from "../../Lib/PerformRequest";
 import { Endpoints } from "../../Lib/Endpoints";
-import {
-  DefaultResponse,
-  GetProductsResponse,
-  LoginResponse,
-  UploadFileResponse,
-} from "../../Lib/Responses";
+import { DefaultResponse } from "../../Lib/Responses";
 import MegaLoader from "../../Misc/MegaLoader";
 import { AppContext } from "../DashboardContainer";
 import ProgressCircle from "../../Misc/ProgressCircle";
-import {
-  getFinancialValueFromNumeric,
-  validateEmail,
-  validatePhoneNumber,
-} from "../../Lib/Methods";
+import { getFinancialValueFromNumeric } from "../../Lib/Methods";
 
 import "./styles.scss";
 
@@ -165,130 +154,150 @@ export default function Wallet() {
                 </center>
               )}
             </div>
-            <div className="flex-row width-100 justify-between actions">
-              <CopyToClipboard
-                text={
-                  userContext.wallet?.virtual_account.virtual_account_no ?? " "
-                }
-                onCopy={() => {
-                  removeAllToasts();
-                  addToast("Account copied to clipboard!", {
-                    appearance: "success",
-                  });
-                }}
-              >
-                <div className="flex-col justify-between bank">
-                  <span className="text-darker px-16 fw-500 label">
-                    Virtual Account Details
-                  </span>
+            {/* <div className="flex-row width-100 justify-between actions"> */}
+            <Grid
+              container
+              className="actions"
+              justifyContent="space-between"
+              spacing={3}
+            >
+              <Grid item className="actions-grid-item">
+                <CopyToClipboard
+                  text={
+                    userContext.wallet?.virtual_account.virtual_account_no ??
+                    " "
+                  }
+                  onCopy={() => {
+                    removeAllToasts();
+                    addToast("Account copied to clipboard!", {
+                      appearance: "success",
+                    });
+                  }}
+                >
+                  <div className="flex-col justify-between bank">
+                    <span className="text-darker px-16 fw-500 label">
+                      Virtual Account Details
+                    </span>
+                    <div className="flex-row align-center">
+                      <span className="icon px-20 flex-row align-center justify-center">
+                        <i className="far fa-university" />
+                      </span>
+                      &nbsp; &nbsp;
+                      <span className="name">Access Bank</span>
+                    </div>
+                    <span className="account">Ajiboye Oluwaferanmi</span>
+                    <span className="number text-blue-default">1219883420</span>
+                  </div>
+                </CopyToClipboard>
+              </Grid>
+              <Grid item className="actions-grid-item">
+                <div className="flex-col justify-between transfer">
                   <div className="flex-row align-center">
-                    <span className="icon px-20 flex-row align-center justify-center">
-                      <i className="far fa-university" />
+                    <span className="text-white px-16 fw-500 label">
+                      Wallet Transfer
                     </span>
                     &nbsp; &nbsp;
-                    <span className="name">Access Bank</span>
-                  </div>
-                  <span className="account">Ajiboye Oluwaferanmi</span>
-                  <span className="number text-blue-default">1219883420</span>
-                </div>
-              </CopyToClipboard>
-              <div className="flex-col justify-between transfer">
-                <div className="flex-row align-center">
-                  <span className="text-white px-16 fw-500 label">
-                    Wallet Transfer
-                  </span>
-                  &nbsp; &nbsp;
-                  <span className="icon px-20 flex-row align-center justify-center text-white">
-                    <i className="far fa-shopping-bag" />
-                  </span>
-                </div>
-
-                <input
-                  type="text"
-                  placeholder="Input Wallet ID"
-                  className="input"
-                />
-                <input
-                  type="number"
-                  placeholder="Enter Amount"
-                  className="input"
-                />
-                <button className="pay">Continue</button>
-              </div>
-              <div className="flex-col justify-between user-bank">
-                <div className="flex-row width-100 justify-between align-center">
-                  <span className="text-darker px-16 fw-500 label">
-                    Bank Details
-                  </span>
-                  <span
-                    className="text-darker px-16 fw-500 text-green-primary flex-row align-center pointer"
-                    onClick={() => {
-                      setEditBankDetails(!isEditBankDetails);
-                    }}
-                  >
-                    {isEditBankDetails ? (
-                      <>Cancel &nbsp;</>
-                    ) : (
-                      <>
-                        Edit &nbsp;
-                        <i className="far fa-pencil" />
-                      </>
-                    )}
-                  </span>
-                </div>
-                {isEditBankDetails ? (
-                  <>
-                    <input
-                      type="number"
-                      className="number"
-                      placeholder="Account number"
-                      maxLength={10}
-                      value={bankForm.number}
-                      onChange={(e) => {
-                        setBankForm({ ...bankForm, number: e.target.value });
-                      }}
-                    />
-                    <Select
-                      placeholder="Select Bank"
-                      label="Select Bank"
-                      size="small"
-                      value={bankForm.bankCode}
-                      onChange={(e) => {
-                        setBankForm({ ...bankForm, bankCode: e.target.value });
-                      }}
-                    >
-                      <MenuItem value="">Select Bank</MenuItem>
-                      {userContext.banks.map((bank) => {
-                        return (
-                          <MenuItem value={bank.bank_code}>
-                            {bank.bank_name}
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                    <button
-                      className="submit"
-                      type="button"
-                      onClick={() => UpdateBankAccount()}
-                      disabled={isBankUploading}
-                    >
-                      {isBankUploading ? <ProgressCircle /> : "Submit"}
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <div className="flex-row align-center">
-                      <span className="name">
-                        {getUserBankInformation("name")}
-                      </span>
-                    </div>
-                    <span className="text-blue-default">
-                      {getUserBankInformation("number")}
+                    <span className="icon px-20 flex-row align-center justify-center text-white">
+                      <i className="far fa-shopping-bag" />
                     </span>
-                  </>
-                )}
-              </div>
-            </div>
+                  </div>
+
+                  <input
+                    type="text"
+                    placeholder="Input Wallet ID"
+                    className="input"
+                  />
+                  <input
+                    type="number"
+                    placeholder="Enter Amount"
+                    className="input"
+                  />
+                  <button className="pay">Continue</button>
+                </div>
+              </Grid>
+              <Grid item className="actions-grid-item">
+                <div className="flex-col justify-between user-bank">
+                  <div className="flex-row width-100 justify-between align-center">
+                    <span className="text-darker px-16 fw-500 label">
+                      Bank Details
+                    </span>
+                    <span
+                      className="text-darker px-16 fw-500 text-green-primary flex-row align-center pointer"
+                      onClick={() => {
+                        setEditBankDetails(!isEditBankDetails);
+                      }}
+                    >
+                      {isEditBankDetails ? (
+                        <>Cancel &nbsp;</>
+                      ) : (
+                        <>
+                          Edit &nbsp;
+                          <i className="far fa-pencil" />
+                        </>
+                      )}
+                    </span>
+                  </div>
+                  {isEditBankDetails ? (
+                    <>
+                      <input
+                        type="number"
+                        className="number"
+                        placeholder="Account number"
+                        maxLength={10}
+                        value={bankForm.number}
+                        onChange={(e) => {
+                          setBankForm({
+                            ...bankForm,
+                            number: e.target.value,
+                          });
+                        }}
+                      />
+                      <Select
+                        placeholder="Select Bank"
+                        label="Select Bank"
+                        size="small"
+                        value={bankForm.bankCode}
+                        onChange={(e) => {
+                          setBankForm({
+                            ...bankForm,
+                            bankCode: e.target.value,
+                          });
+                        }}
+                      >
+                        <MenuItem value="">Select Bank</MenuItem>
+                        {userContext.banks.map((bank) => {
+                          return (
+                            <MenuItem value={bank.bank_code}>
+                              {bank.bank_name}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                      <button
+                        className="submit"
+                        type="button"
+                        onClick={() => UpdateBankAccount()}
+                        disabled={isBankUploading}
+                      >
+                        {isBankUploading ? <ProgressCircle /> : "Submit"}
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <div className="flex-row align-center">
+                        <span className="name">
+                          {getUserBankInformation("name")}
+                        </span>
+                      </div>
+                      <span className="text-blue-default">
+                        {getUserBankInformation("number")}
+                      </span>
+                    </>
+                  )}
+                </div>
+              </Grid>
+            </Grid>
+            {/* </div> */}
           </>
         ) : (
           <MegaLoader />
